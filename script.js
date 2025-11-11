52e13d72b5294ea52b5ef9e98f8fe7b181974a82
jainarchi <imarchijain2405@gmail.com>
2025-11-12 00:41:48 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+let board = document.getElementById("s2");
+
+let cellWidth = 80;
+let cellHeight = 80;
+
+let row = Math.floor(board.clientHeight / cellHeight);
+let col = Math.floor(board.clientWidth / cellWidth);
 
 
+
+for (let i = 0; i < row; i++) {
+  for (let j = 0; j < col; j++) {
+    
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
+  }
+}
