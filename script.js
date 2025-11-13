bec635c86dcd9a54ffa911469d2c30d09c907b0f
jainarchi <imarchijain2405@gmail.com>
2025-11-13 22:32:05 +0530

    done

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 let board = document.getElementById("s2");
-let startBtn = document.querySelector(".startBtn")
-let startGame = document.querySelector(".startGame")
-let overGame = document.querySelector(".overGame")
-let restartBtn = document.querySelector(".restartBtn")
+let startBtn = document.querySelector(".startBtn");
+let startGame = document.querySelector(".startGame");
+let overGame = document.querySelector(".overGame");
+let restartBtn = document.querySelector(".restartBtn");
 
-let showScore = document.getElementById("score")
-let showHighScore = document.getElementById("highScore")
-let showTime = document.getElementById("time") 
+let timeInterval = null;
+let interval = null;
+let food = null;
+let dir = "null";  
 
+let showScore = document.getElementById("score");
+let showHighScore = document.getElementById("highScore");
+let showTime = document.getElementById("time");
 
-let highScore = parseInt(localStorage.getItem('highScore')) || 0;
-let score = 0 ;
+let highScore = parseInt(localStorage.getItem("highScore")) || 0;
+let score = 0;
 let time = `00-00`;
-
 
 let cellWidth = 80;
 let cellHeight = 80;
-let row = Math.floor(board.clientHeight / cellHeight);
-let col = Math.floor(board.clientWidth / cellWidth);
+let row = 0 ;
+let col = 0;
 
-let timeInterval = null ;
-let interval = null;
-let food = null;
+let mediaQuery = window.matchMedia("(max-width: 650px)");
+
+function updateCellDimensions(mql) {
+  if (mql.matches) {
+    cellWidth = 60;
+    cellHeight = 60;
+  } else {
+    cellWidth = 80;
+    cellHeight = 80;
+  }
+
+   row = Math.floor(board.clientHeight / cellHeight);
+   col = Math.floor(board.clientWidth / cellWidth);
+
+  console.log(`Dimensions: ${cellWidth}x${cellHeight}. Grid: ${row}x${col}`
+  );
+}
+
+updateCellDimensions(mediaQuery);
+mediaQuery.addEventListener("change", updateCellDimensions);
 
 const blocks = [];
 let snake = [{ x: 1, y: 3 }];
@@ -34,6 +54,8 @@ for (let i = 0; i < row; i++) {
     blocks[`${i}-${j}`] = cell;
   }
 }
+
+
 
 function randomFood() {
   food = {
@@ -51,16 +73,14 @@ function render() {
 }
 
 function run() {
-
   interval = setInterval(() => {
     let head = snake[0];
 
     if (head.x < 0 || head.x === row || head.y < 0 || head.y === col) {
-      overGame.style.display = "flex"
+      overGame.style.display = "flex";
       clearInterval(interval);
       return;
-    } 
-    else {
+    } else {
       if (dir === "ArrowRight") {
         head = { x: snake[0].x, y: snake[0].y + 1 };
       } else if (dir === "ArrowLeft") {
@@ -76,16 +96,15 @@ function run() {
         randomFood();
         snake.unshift(head);
 
-        score += 10 ;
-        showScore.textContent = score ;
+        score += 10;
+        showScore.textContent = score;
 
-        if(highScore < score){
-            highScore = score
-            showHighScore.textContent = highScore ;
+        if (highScore < score) {
+          highScore = score;
+          showHighScore.textContent = highScore;
 
-            localStorage.setItem('highScore' ,  highScore)
+          localStorage.setItem("highScore", highScore);
         }
-        
       }
 
       snake.unshift(head);
@@ -100,66 +119,60 @@ function run() {
 }
 
 
-startBtn.addEventListener("click" , () =>{
-    startGame.style.display = "none"
+// start
 
-    showHighScore.textContent = highScore ;
-     
-    timeInterval = setInterval(()=>{
-          let [min , sec] = time.split("-").map(Number);
-           
-          if(sec === 59){
-            min += 1 ;
-            sec = 0 ;
-          }
-          else {
-            sec += 1
-          }
-          
-          time = `${min}-${sec}`
-          showTime.textContent = time ; 
-    } , 1000)
+startBtn.addEventListener("click", () => {
+  startGame.style.display = "none";
 
-   
-  }) 
+  showHighScore.textContent = highScore;
 
+  timeInterval = setInterval(() => {
+    let [min, sec] = time.split("-").map(Number);
 
+    if (sec === 59) {
+      min += 1;
+      sec = 0;
+    } else {
+      sec += 1;
+    }
 
-let dir = "null";
-randomFood();            // for intial food
-render();                // for intial snake
+    time = `${min}-${sec}`;
+    showTime.textContent = time;
+  }, 1000);
 
-
-
-addEventListener("keydown", (e) => {
-  dir = e.key;
-  if(interval === null){            // otherwise interval already set
-     run();
-  }
-              
+  randomFood();        // for intial food
+  render();
 });
 
 
-restartBtn.addEventListener("click" , () =>{
-     overGame.style.display="none"
-     
-     snake.shift();
-     
-     snake.forEach((obj)=>{
-       console.log(obj);
-       blocks[`${obj.x}-${obj.y}`].classList.remove("fill");
-     })
-      blocks[`${food.x}-${food.y}`].classList.remove("food");
+   
 
-     dir = "null";
-     interval = null;
+addEventListener("keydown", (e) => {
+  dir = e.key;
+  if (interval === null) {          // otherwise interval already set
+    run();
+  }
+});
 
 
-     snake = [{ x: 1, y: 3 }]
-     render()
-     randomFood();
-     score = 0 ;
-     showScore.textContent = score ;
 
-     
-})
+restartBtn.addEventListener("click", () => {
+  overGame.style.display = "none";
+
+  snake.shift();
+
+  snake.forEach((obj) => {
+    console.log(obj);
+    blocks[`${obj.x}-${obj.y}`].classList.remove("fill");
+  });
+  blocks[`${food.x}-${food.y}`].classList.remove("food");
+
+  dir = "null";
+  interval = null;
+
+  snake = [{ x: 1, y: 3 }];
+  render();
+  randomFood();
+  score = 0;
+  showScore.textContent = score;
+});
